7601537e87dad74f1f5a5ada9a737df5ce2ee3ae
Rixafy <rixafy@gmail.com>
2019-04-06 22:57:50 +0200

    Add and implement Admin DTO and Admin::onLogin event

--- app/Model/Admin/Admin.php
+++ app/Model/Admin/Admin.php
@@ -50,6 +50,31 @@ class Admin
      */
     private $last_login_at;
 
+    public function __construct(AdminData $adminData)
+    {
+        $this->display_name = $adminData->displayName;
+        $this->email = $adminData->email;
+    }
+
+    public function onLogin(): void
+    {
+        $this->last_login_at = new DateTime();
+        $this->last_ip = $_SERVER['REMOTE_ADDR'];
+
+        if ($this->first_ip === null) {
+            $this->first_ip = $this->last_ip;
+        }
+
+        if ($this->first_login_at === null) {
+            $this->first_login_at = $this->last_login_at;
+        }
+    }
+
+    public function createPassword(string $password, callable $hash): void
+    {
+        $this->password = $hash($password);
+    }
+
     public function getDisplayName(): string
     {
         return $this->display_name;
---
+++ app/Model/Admin/AdminData.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Minecord\Model\Admin;
+
+use DateTime;
+
+class AdminData
+{
+    /** @var string */
+    public $displayName;
+
+    /** @var string */
+    public $email;
+
+}
