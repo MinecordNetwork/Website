75258b3cd451200c86df8d467d39a194d6da560b
Rixafy <rixafy@gmail.com>
2020-05-02 06:22:18 +0200

    Add basic design

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+async function copyIpAddress(str, strDefault, strCopied, element) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    element.innerHTML = strCopied;
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    element.innerHTML = strDefault;
+}
