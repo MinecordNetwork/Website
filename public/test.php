d777449e77ed8ffafc3f7c67bd8c67cec0ba8cff
Rixafy <rixafy@gmail.com>
2020-05-13 20:24:48 +0200

    Remove bootstrap tooltip from online maps

--- public/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$content = file_get_contents('https://surv.minecord.net/index.html');
-$content = str_replace('src="', 'src="https://surv.minecord.net/', $content);
-$content = str_replace('href="', 'href="https://surv.minecord.net/', $content);
-echo $content;
