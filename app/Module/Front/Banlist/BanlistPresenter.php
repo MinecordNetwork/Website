f18655e2037a51d5b143793f9180947860aa7fef
Rixafy <rixafy@gmail.com>
2020-06-04 03:44:16 +0200

    Add unban payments to banlist page

--- app/Module/Front/Banlist/BanlistPresenter.php
+++ app/Module/Front/Banlist/BanlistPresenter.php
@@ -5,7 +5,17 @@ declare(strict_types=1);
 namespace Minecord\Module\Front\Banlist;
 
 use Minecord\Model\Banlist\BanlistFacade;
+use Minecord\Model\Payment\PayPal\PayPalCredentials;
+use Minecord\Model\Payment\PayPal\PayPalPaymentDataFactory;
+use Minecord\Model\Payment\PayPal\PayPalPaymentFacade;
+use Minecord\Model\Product\Exception\ProductNotFoundException;
+use Minecord\Model\Product\ProductFacade;
 use Minecord\Module\Front\BaseFrontPresenter;
+use Minecord\Module\Front\Vip\Form\VipProductFormFactory;
+use Nette\Application\UI\Form;
+use Nette\Application\UI\Multiplier;
+use Ramsey\Uuid\Nonstandard\Uuid;
+use Rixafy\Currency\CurrencyFacade;
 
 /**
  * @property-read BanlistTemplate $template
@@ -13,12 +23,30 @@ use Minecord\Module\Front\BaseFrontPresenter;
 class BanlistPresenter extends BaseFrontPresenter
 {
 	private BanlistFacade $banlistFacade;
+	private ProductFacade $productFacade;
+	private CurrencyFacade $currencyFacade;
+	private PayPalCredentials $payPalCredentials;
+	private VipProductFormFactory $vipProductFormFactory;
+	private PayPalPaymentFacade $payPalPaymentFacade;
+	private PayPalPaymentDataFactory $payPalPaymentDataFactory;
 
 	public function __construct(
-		BanlistFacade $banlistFacade
+		BanlistFacade $banlistFacade,
+		ProductFacade $productFacade,
+		CurrencyFacade $currencyFacade,
+		PayPalCredentials $payPalCredentials,
+		VipProductFormFactory $vipProductFormFactory,
+		PayPalPaymentFacade $payPalPaymentFacade, 
+		PayPalPaymentDataFactory $payPalPaymentDataFactory
 	) {
 		parent::__construct();
 		$this->banlistFacade = $banlistFacade;
+		$this->productFacade = $productFacade;
+		$this->currencyFacade = $currencyFacade;
+		$this->payPalCredentials = $payPalCredentials;
+		$this->vipProductFormFactory = $vipProductFormFactory;
+		$this->payPalPaymentFacade = $payPalPaymentFacade;
+		$this->payPalPaymentDataFactory = $payPalPaymentDataFactory;
 	}
 	
 	public function actionDefault(int $page = 1): void
@@ -35,5 +63,35 @@ class BanlistPresenter extends BaseFrontPresenter
 		$this->template->page = $page;
 		$this->template->pageCount = $pageCount;
 		$this->template->bans = $this->banlistFacade->getAll($itemsPerPage, ($page - 1) * $itemsPerPage);
+
+		try {
+			$this->template->unBan = $this->productFacade->getBySmsCodeAndPriceSlovak('UNBAN', 6);
+		} catch (ProductNotFoundException $e) {
+			$this->template->unBan = null;
+		}
+	}
+
+	public function renderDefault(): void
+	{
+		$this->template->payPalClientId = $this->payPalCredentials->getClientId();
+		$this->template->czkCurrency = $this->currencyFacade->getByCode('CZK');
+	}
+
+	public function handleSuccess(): void
+	{
+		$this->template->paymentSuccessful = true;
+		$this->redrawControl('productSnippet');
+	}
+
+	protected function createComponentProductForm(): Form
+	{
+		$form = $this->vipProductFormFactory->create($this->template->unBan->getId());
+
+		$form->onSuccess[] = function (Form $form, array $data) {
+			$this->template->payPalPayment = $this->payPalPaymentFacade->create($this->payPalPaymentDataFactory->createFromFormData($data));
+			$this->redrawControl('productSnippet');
+		};
+
+		return $form;
 	}
 }
--- app/Module/Front/Banlist/BanlistTemplate.php
+++ app/Module/Front/Banlist/BanlistTemplate.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Minecord\Module\Front\Banlist;
 
 use Minecord\Model\Banlist\Ban;
+use Minecord\Model\Payment\PayPal\PayPalPayment;
+use Minecord\Model\Product\Product;
 use Minecord\Module\Front\BaseFrontTemplate;
+use Rixafy\Currency\Currency;
 
 class BanlistTemplate extends BaseFrontTemplate
 {
@@ -13,4 +16,9 @@ class BanlistTemplate extends BaseFrontTemplate
 	public array $bans;
 	public int $page;
 	public int $pageCount;
+	public ?Product $unBan = null;
+	public string $payPalClientId;
+	public Currency $czkCurrency;
+	public ?PayPalPayment $payPalPayment = null;
+	public bool $paymentSuccessful = false;
 }
