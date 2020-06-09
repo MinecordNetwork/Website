a2877319f458ec01cb8204324b9f961c08f375f5
Rixafy <rixafy@gmail.com>
2020-06-09 15:48:27 +0200

    Add js optimizations, remove fawesome

--- public/js/main.js
+++ public/js/main.js
@@ -17,8 +17,19 @@ document.addEventListener('DOMContentLoaded', function() {
     naja.initialize({
         history: false
     });
-    
+
     global.naja = naja
+
+    const _paq = window._paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        const u = "//analytics.rixafy.pro/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', window.location.hostname === 'minecord.net' ? 6 : 5]);
+        const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
 });
 
 async function copyIpAddress(str, strDefault, strCopied, element) {
