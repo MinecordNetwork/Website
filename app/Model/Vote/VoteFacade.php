ecfba5eef2eb9e9087961c6c4364fe12fa75431c
Rixafy <rixafy@gmail.com>
2020-07-03 22:00:58 +0200

    Ignore votes from MCSL serverlist

--- app/Model/Vote/VoteFacade.php
+++ app/Model/Vote/VoteFacade.php
@@ -19,7 +19,7 @@ class VoteFacade
 
 	public function getCount(): int
 	{
-		return (int) $this->database->query('SELECT id FROM minecraft_ban WHERE is_active = 1')->getRowCount();
+		return (int) $this->database->query('SELECT id FROM minecraft_vote')->getRowCount();
 	}
 
 	/** 
@@ -29,7 +29,7 @@ class VoteFacade
 	{
 		$voteStats = [];
 
-		$result = $this->database->query('SELECT COUNT(minecraft_vote.id) as vote_count, minecraft_player.name as player_name, MAX(minecraft_vote.created_at) as last_vote FROM minecraft_vote JOIN minecraft_player ON minecraft_vote.player_id = minecraft_player.id WHERE YEAR(minecraft_vote.created_at) = ? AND MONTH(minecraft_vote.created_at) = ? GROUP BY minecraft_vote.player_id ORDER BY vote_count DESC LIMIT ?', $year, $month, $limit);
+		$result = $this->database->query('SELECT COUNT(minecraft_vote.id) as vote_count, minecraft_player.name as player_name, MAX(minecraft_vote.created_at) as last_vote FROM minecraft_vote JOIN minecraft_player ON minecraft_vote.player_id = minecraft_player.id WHERE YEAR(minecraft_vote.created_at) = ? AND MONTH(minecraft_vote.created_at) = ? AND minecraft_vote.serverlist != ? GROUP BY minecraft_vote.player_id ORDER BY vote_count DESC LIMIT ?', $year, $month, 'MCSL', $limit);
 
 		$rank = 1;
 		foreach ($result as $row) {
