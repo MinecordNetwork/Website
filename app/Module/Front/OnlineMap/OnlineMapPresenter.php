ed02d0949436c812d248f5fe6f63286eaec0cd1f
Rixafy <rixafy@gmail.com>
2021-12-24 04:23:17 +0100

    Chane online map url

--- app/Module/Front/OnlineMap/OnlineMapPresenter.php
+++ app/Module/Front/OnlineMap/OnlineMapPresenter.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace App\Module\Front\OnlineMap;
 
 use App\Module\Front\BaseFrontPresenter;
+use Nette\Application\AbortException;
 
 /**
  * @property OnlineMapTemplate $template
  */
 class OnlineMapPresenter extends BaseFrontPresenter
 {
-
+    /**
+     * @throws AbortException
+     */
+    public function actionSurvival(): void
+    {
+        $this->redirectUrl('https://survival.' . $this->getHttpRequest()->getUrl()->getHost());
+    }
 }
